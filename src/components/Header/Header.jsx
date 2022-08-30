4c6474fbdabc734c475326316dfe5092d043460a
Pankaj Raj <75722183+Ipankaj07@users.noreply.github.com>
2022-08-30 22:54:49 +0530

    Added Navigation link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import "./header.css";
+import { Link } from "react-router-dom";
+import { ImCart } from "react-icons/im";
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <div className="header">
+      <div className="navbar">
+        <div className="navbar__logo">
+          <Link to="/">
+            <div className="logo">
+              <ImCart size={25} />
+            </div>
+          </Link>
+          <span></span>
+        </div>
+        <div className="navbar__links">
+          <Link to="/" className="navbar__link">
+            Home
+          </Link>
+          <Link to="/login" className="navbar__link">
+            Login
+          </Link>
+          <Link to="/signup" className="navbar__link">
+            SignUp
+          </Link>
+          <Link to="/cart" className="navbar__link">
+            Cart
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
