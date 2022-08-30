ce2db8df0db5ecc0bbea6021d4527d98eec7c1e6
Pankaj Raj <75722183+Ipankaj07@users.noreply.github.com>
2022-08-30 22:53:18 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { ChakraProvider } from "@chakra-ui/react";
 
+import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
@@ -12,11 +12,11 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ChakraProvider>
-      {/* <Provider store={store}> */}
+      <Provider store={store}>
         <BrowserRouter>
           <App />
         </BrowserRouter>
-      {/* </Provider> */}
+      </Provider>
     </ChakraProvider>
   </React.StrictMode>
 );
