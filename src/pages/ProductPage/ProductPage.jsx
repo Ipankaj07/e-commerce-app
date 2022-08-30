2df22dd0c7dfe43691786d3fe5a8726fee3f5cc1
Pankaj Raj <75722183+Ipankaj07@users.noreply.github.com>
2022-08-30 22:55:03 +0530

    Update ProductPage.jsx

--- src/pages/ProductPage/ProductPage.jsx
+++ src/pages/ProductPage/ProductPage.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import ProductList from "../../components/ProductList/ProductList";
 
 function ProductPage() {
   return (
     <div>
-        <h1>Product Page</h1>
+      <ProductList />
     </div>
   );
 }
