5ceac6efe037da555e345d08c85bb87fc5d6cf38
Pankaj Raj <75722183+Ipankaj07@users.noreply.github.com>
2022-08-30 22:51:37 +0530

    Update Signup.jsx

--- src/components/User/Signup/Signup.jsx
+++ src/components/User/Signup/Signup.jsx
@@ -1,81 +1,97 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
-// import { signupUserData } from "../../../redux/actions/userAction";
+import Loading from "../../Accessory/Loading";
+
+import { signupUser } from "../../../redux/actions/userAction";
 
 function SignUp() {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // dispatch(signupUserData(firstName, lastName, email, password));
-    setFirstName("");
-    setLastName("");
-    setEmail("");
-    setPassword("");
+    dispatch(signupUser({ firstName, lastName, email, password }));
+    setLoading(true);
     setTimeout(() => {
+      setFirstName("");
+      setLastName("");
+      setEmail("");
+      setPassword("");
+      setError("");
+      setLoading(false);
       navigate("/");
-    }, 1000);
+    }, 1500);
   };
 
+  useEffect(() => {
+    if (localStorage.getItem("userId")) {
+      navigate("/");
+    }
+  }, [localStorage.getItem("userId"), navigate]);
+
   return (
-    <div
-      style={{ backgroundColor: "#fff", minHeight: "90vh", marginTop: "-24px" }}
-    >
-      <div className="comm-header">
-        <section className="login__section">
-          <div className="login__div">
-            <p className=" login__heading">Register</p>
-            <form onSubmit={handleSubmit}>
-              <input
-                type="text"
-                placeholder="First Name"
-                value={firstName}
-                onChange={(e) => setFirstName(e.target.value)}
-              />
-              <input
-                type="text"
-                placeholder="Last Name"
-                value={lastName}
-                onChange={(e) => setLastName(e.target.value)}
-              />
-              <input
-                type="email"
-                placeholder="Email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              <input
-                type="password"
-                placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              <div>
-                <button type="submit">Register</button>
-              </div>
-              <div className="l__fot dis-flex">
-                <div>If you already have an account, please</div>
-                <div
-                  className="navig__signup"
-                  onClick={() => {
-                    navigate("/login");
-                  }}
-                >
-                  Login
+    <div style={{ backgroundColor: "#fff", marginTop: "24px" }}>
+      {loading ? (
+        <Loading />
+      ) : (
+        <div className="comm-header">
+          <div>{error}</div>
+          <section className="login__section">
+            <div className="login__div">
+              <p className=" login__heading">Register</p>
+              <form onSubmit={handleSubmit}>
+                <input
+                  type="text"
+                  placeholder="First Name"
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
+                />
+                <input
+                  type="text"
+                  placeholder="Last Name"
+                  value={lastName}
+                  onChange={(e) => setLastName(e.target.value)}
+                />
+                <input
+                  type="email"
+                  placeholder="Email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <input
+                  type="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <div>
+                  <button type="submit">Register</button>
                 </div>
-              </div>
-            </form>
-          </div>
-        </section>
-      </div>
+                <div className="l__fot dis-flex">
+                  <div>If you already have an account, please</div>
+                  <div
+                    className="navig__signup"
+                    onClick={() => {
+                      navigate("/login");
+                    }}
+                  >
+                    Login
+                  </div>
+                </div>
+              </form>
+            </div>
+          </section>
+        </div>
+      )}
     </div>
   );
 }
